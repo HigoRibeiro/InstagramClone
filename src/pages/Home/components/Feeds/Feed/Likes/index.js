8376d282f8cc2bac474b5cc0432494ef345719f7
Higo Ribeiro <HORBJN@gmail.com>
2018-04-27 13:50:17 -0300

    Component Likes do Feed estilizado

--- src/pages/Home/components/Feeds/Feed/Likes/index.js
+++ src/pages/Home/components/Feeds/Feed/Likes/index.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
-import {  View, Text, } from 'react-native';
+import {  View, Text, StyleSheet } from 'react-native';
 
 export default class Likes extends Component {
   render() {
     return (
-      <View>
-        <Text> Likes </Text>
+      <View style={styles.container}>
+        <Text style={styles.text}>8 curtidas</Text>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 10,
+  },
+  text: {
+    fontSize: 13,
+    color: '#222',
+    fontWeight: 'bold',
+  }
+});
