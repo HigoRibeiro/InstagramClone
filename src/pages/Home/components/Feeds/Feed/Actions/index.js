bf9ec1b37d024bed2f3da9ba2f7a10e0142aff8d
Higo Ribeiro <HORBJN@gmail.com>
2018-04-27 13:46:05 -0300

    Component Actions do Feed estilizado

--- src/pages/Home/components/Feeds/Feed/Actions/index.js
+++ src/pages/Home/components/Feeds/Feed/Actions/index.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react';
-import {  View, Text, } from 'react-native';
+import {  View, Text, StyleSheet } from 'react-native';
+
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default class Actions extends Component {
   render() {
     return (
-      <View>
-        <Text> Actions </Text>
+      <View style={styles.container}>
+        <View style={styles.containerLeft} >
+          <Icon name="heart-o" size={20} color="#222" />
+          <Icon name="comment-o" size={20} color="#222" />
+          <Icon name="angle-double-right" size={20} color="#222" />
+        </View>
+        <Icon name="bookmark-o" size={20} color="#222" />
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingHorizontal: 10,
+    height: 40,
+  },
+  containerLeft: {
+    flexDirection: 'row',
+    width: 78,
+    justifyContent: 'space-between'
+  }
+});
